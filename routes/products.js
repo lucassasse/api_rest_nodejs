c70f9bc2e2f70e104abfdad91fedf35d50f6034c
Lucas Eduardo Sasse <lucas1sasse@gmail.com>
2023-09-09 14:47:32 -0300

    Elaborada documentação interna da API

--- routes/products.js
+++ routes/products.js
@@ -9,7 +9,22 @@ router.get('/', (req, res, next) => {
 			'SELECT * FROM products;',
 			(error, result, field) => {
 				if(error){ return res.status(500).send({error: error}) }
-				return res.status(200).send({response: result});
+				const response = {
+					quantity: result.length,
+					product: result.map(prod => {
+						return {
+							id_products: prod.id_products,
+							name: prod.name,
+							price: prod.price,
+							request: {
+								type: 'GET',
+								description: 'Return a products details',
+								url: 'http://localhost:3000/products/' + prod.id_products
+							}
+						}
+					})
+				}
+				return res.status(200).send({response});
 			}
 		)
 	});
@@ -24,11 +39,21 @@ router.post('/', (req, res, next) => {
 			(error, result, field) => {
 				conn.release(); //liberar rotas para não travar o servidor
 				if(error){ return res.status(500).send({error: error}) }
-
-				res.status(201).send({
+				const response = {
 					message: 'Produto Inserido com Sucesso!',
-					productCreate: result
-				});
+					productCreate: {
+						id_products: result.id_products,
+						name: req.body.name,
+						price: req.body.price,
+						request: {
+							type: 'POST',
+							description: 'Insert a product',
+							url: 'http://localhost:3000/products/'
+						}
+					}
+				}
+
+				return res.status(201).send(response);
 			}
 		)
 	});
@@ -42,7 +67,26 @@ router.get('/:id_product', (req, res, next) => {
 			[req.params.id_product],
 			(error, result, field) => {
 				if(error){ return res.status(500).send({error: error}) }
-				return res.status(200).send({response: result});
+
+				if(result.length == 0){
+					return res.status(404).send({
+						message: 'Not Found Product with this ID'
+					})
+				}
+
+				const response = {
+					product: {
+						id_products: result[0].id_products,
+						name: result[0].name,
+						price: result[0].price,
+						request: {
+							type: 'GET',
+							description: 'Return a product by id',
+							url: 'http://localhost:3000/products/'
+						}
+					}
+				}
+				return res.status(200).send(response);
 			}
 		)
 	});
@@ -65,9 +109,21 @@ router.patch('/', (req, res, next) => {
 				conn.release(); //liberar rotas para não travar o servidor
 				if(error){ return res.status(500).send({error: error}) }
 
-				res.status(202).send({
+				const response = {
 					message: 'Produto Alterado com Sucesso!',
-				});
+					productUpdated: {
+						id_products: req.body.id_products,
+						name: req.body.name,
+						price: req.body.price,
+						request: {
+							type: 'PATCH',
+							description: 'Update a product',
+							url: 'http://localhost:3000/products/' + req.body.id_products
+						}
+					}
+				}
+
+				return res.status(202).send(response);
 			}
 		)
 	});
@@ -83,9 +139,20 @@ router.delete('/', (req, res, next) => {
 				conn.release(); //liberar rotas para não travar o servidor
 				if(error){ return res.status(500).send({error: error}) }
 
-				res.status(202).send({
+				const response = {
 					message: 'Produto Excluído com Sucesso!',
-				});
+					request: {
+						type: 'POST',
+						description: 'Delete/Insert a product',
+						url: 'http://localhost:3000/products/',
+						body: {
+							name: 'String',
+							price: 'Number'
+						}
+					}
+				}
+
+				return res.status(202).send(response);
 			}
 		)
 	});
